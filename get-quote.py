966709cd04ff492f7508cedc40494159cc4ed8b0
meir-cg <58570185+meir-cg@users.noreply.github.com>
2019-12-06 09:43:46 -0800

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,5 @@
+import random
+
 def primary():
   #print("Keep it logically awesome.")
 
@@ -5,7 +7,13 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  print(quotes[-1])
+  last = len(quotes) -1
+  rnd = random.randint(0, last)
+
+  print(last)
+
+  print(quotes[rnd])
+  print(quotes[1])
 
 if __name__== "__main__":
   primary()
